9d482d97680d885a57d56b149651c2f61c82fc82
Anselmo Oliveira <anselmoliveira@gmail.com>
2024-06-15 23:48:46 -0300

    "Updated ContaTerminal.class binary file"

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+
+        int numero = Integer.valueOf(args [0]);
+        String agencia = args [1];
+        String nomeCliente = args[2];
+        double saldo = Double.valueOf(args[3]);
+
+        //TODO: Conhecer e importar a classe Scanner
+
+
+        //TODO: Exibir as mensagens para o nosso usuário
+
+
+        //TODO: Obter, pela classe Scanner, os valores digitados no terminal
+
+
+        //TODO: Exibir a mensagem da conta criada
+
+        System.out.println("Olá, " " + nomeCliente + "" obrigado por criar uma conta em nosso banco, 
+        sua agência é "" + agencia + "", conta "" + numero + "" e seu saldo "" + saldo + "
+        " já está disponível para saque. Parabéns!");
+
+
+
     }
 }
